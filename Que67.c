e83e2bb58ea9d372fa803b9e7fa8e80c126de114
Dikshita621 <dikshitachoudhary845@gmail.com>
2025-09-30 20:31:19 +0530

    Create Que67.c

---
+++ Que67.c
@@ -0,0 +1,42 @@
+//Q67: Insert an element in an array at a given position.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+10 20 30 40
+2 15
+Output 1:
+10 20 15 30 40
+
+*/
+  #include <stdio.h>
+
+int main() {
+    int n, i, pos, element;
+    scanf("%d", &n);
+
+    int arr[n + 1];  // Array to hold existing and new element
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d %d", &pos, &element);
+
+    // Shift elements to the right to make space at given position
+    for (i = n; i > pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // Insert the element at position pos
+    arr[pos] = element;
+
+    // Print updated array
+    for (i = 0; i <= n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
